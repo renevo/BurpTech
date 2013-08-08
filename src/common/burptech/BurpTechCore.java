4249d1cb65cd316fec9ea10e427d3a40a96c3c47
Danciu Alex <dersummoner@gmail.com>
2013-08-08 21:11:15 +0300

    Removed un-needed imports.

--- src/common/burptech/BurpTechCore.java
+++ src/common/burptech/BurpTechCore.java
@@ -3,10 +3,8 @@ package burptech;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import burptech.client.gui.GuiPortableWorkbech;
-import burptech.gui.ContainerPortableWorkbench;
 import burptech.gui.GuiHandler;
-import burptech.integration.NeiIntegration;
+import burptech.integration.*;
 import burptech.item.crafting.*;
 import burptech.lib.*;
 import cpw.mods.fml.common.*;
@@ -14,7 +12,6 @@ import cpw.mods.fml.common.Mod.*;
 import cpw.mods.fml.common.event.*;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkRegistry;
-import net.minecraft.inventory.ContainerWorkbench;
 import net.minecraftforge.common.*;
 
 /**
@@ -84,7 +81,6 @@ public class BurpTechCore
         // mod integrations
     	if (configuration.recipePortableWorkbench.getBoolean(true))
     	{
-//    		Nei.registerCraftingContainers(GuiPortableWorkbech.class);
     		NeiIntegration.registerCraftingContainers();
     	}
     }
